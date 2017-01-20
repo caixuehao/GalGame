8d87e1bb2e74b1417eb7921573b2faee80eb69dc
caixuehao <457360691@qq.com>
2017-01-20 18:02:31 +0800

    init

---
+++ Scripts/LoadScene/LoadController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadController : MonoBehaviour {
+	public Sprite[] loadingSpriteArr;
+	public Image loadingImage;
+	public float animationTime = 1.0f;//动画播放时间
+
+	private int currentSpriteIndex = 0;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("changeImage", 0, animationTime / loadingSpriteArr.Length);
+		Invoke("jumpToStart",3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void jumpToStart(){
+		SceneManager.LoadScene ("start");
+	}
+
+	void changeImage(){
+		if (++currentSpriteIndex >= loadingSpriteArr.Length)
+			currentSpriteIndex = 0;
+
+		loadingImage.sprite = loadingSpriteArr [currentSpriteIndex];
+	}
+}
---
+++ Scripts/MainScene/MainController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class MainController : MonoBehaviour {
+
+
+	public Image testButton;
+
+	// Use this for initialization
+	void Start () {
+		UUIEventListener.Get (testButton.gameObject).onClick = jumpToScene1;
+	}
+
+	void jumpToScene1(PointerEventData eventData){
+		SceneManager.LoadScene ("scene1");
+	}
+		
+}
